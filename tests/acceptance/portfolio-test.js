d0a6f7f99b0ab3b5b79cf87c5fe165b9dc3788b4
Ben Adamski <benjadamski@gmail.com>
2017-06-26 12:01:45 -0400

    Add basic acceptance tests
    - These aren't yet complete, but they outline the basic objectives for the app.

--- tests/acceptance/portfolio-test.js
+++ tests/acceptance/portfolio-test.js
@@ -3,10 +3,17 @@ import moduleForAcceptance from 'bengitscode-portfolio/tests/helpers/module-for-
 
 moduleForAcceptance('Acceptance | portfolio');
 
-test('visiting /', function(assert) {
-  visit('/');
+test('should show projects as the home page', function (assert) {
+});
 
-  andThen(function() {
-    assert.equal(currentURL(), '/');
-  });
+test('should link to deployed project.', function (assert) {
+});
+
+test('should link to contact information.', function (assert) {
+});
+
+test('should list all projects.', function (assert) {
+});
+
+test('should show details for a selected project', function (assert) {
 });
