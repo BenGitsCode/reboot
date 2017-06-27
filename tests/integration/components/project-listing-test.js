8881f3cd25b51441a101fbbdf4b3bb542d9506d6
Ben Adamski <benjadamski@gmail.com>
2017-06-27 18:11:46 -0400

    Scaffold out fake object for integration tests
    - For the test we'll pass the component a fake object that has all the properties that our project model has. We'll give the variable the name project, and in each test we'll set project to our local scope, represented by the `this` object.
    - The render template can access values in local scope.

--- tests/integration/components/project-listing-test.js
+++ tests/integration/components/project-listing-test.js
@@ -2,14 +2,25 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import Ember from 'ember'
 
+// Sample fake project object, with all of the same properties as my project model.
+let project = Ember.Object.create({
+  id: 'test-id',
+  title: 'test-title',
+  type: 'test-type',
+  image: 'test-image.png',
+  description: 'test-description'
+
+})
+
 moduleForComponent('project-listing', 'Integration | Component | project listing', {
   integration: true
 });
 
-test('should display project details', function(assert) {
 
+test('should display project details', function (assert) {
+  this.set('projectObj', project)
 });
 
-test('should toggle wide class on click', function(asser) {
-
+test('should toggle wide class on click', function (asser) {
+  this.set('projectObj', project)
 })
